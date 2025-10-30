39f8fa2babaecd119ebee0c5a27f87bae30efece
Sander den Hollander <sander@webenjoyer.com>
2025-10-30 08:57:38 +0100

    Init

---
+++ src/main/java/com/nickmous/beanstash/BeanstashApplication.java
@@ -0,0 +1,13 @@
+package com.nickmous.beanstash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanstashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanstashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nickmous/beanstash/BeanstashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nickmous.beanstash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanstashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
